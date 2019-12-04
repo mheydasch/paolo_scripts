89d7d242af817e417ed4e005e4ffa9ee3362258f
Max Heydasch <max@staff-208-161.eduroam.unibe.ch>
2019-12-04 17:18:13 +0100

    initial commit

---
+++ paolo_concat_experimt_data.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  4 16:42:45 2019
+
+@author: max
+"""
+
+import pandas as pd
+import sys
+import os
+import re
+def parseArguments():
+  # Define the parser and read arguments
+  parser = argparse.ArgumentParser(description='collect segmentation files into one directory')
+  parser.add_argument('-d', '--dir', type=str, help='The directory where the knockdown folders are', required=True)
+  args = parser.parse_args()
+  return(args)
+#%%  
+filesep=os.sep
+path='/Volumes/imaging.data/Paolo/MCF10A_TimeLapse/'
+#%%
+def find_csv(path):
+    findfile='experimentDescription.csv'
+    filelist=[]
+    for root, dirs, files in os.walk(path):
+        if findfile in files:
+            filepath=os.path.join(root, findfile)
+            filelist.append(filepath)
+    return filelist
+
+
+datepattern=re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}')
+def concat_csv(path):
+    filelist=find_csv(path)
+    csvlist=[]
+    for file in filelist:
+        temp=pd.read_csv(file, sep=';')
+        splitpath=file.split(filesep)
+        for i in splitpath:
+            if re.search(datepattern, i)!=None:
+                temp['experiment']=i
+                break
+        csvlist.append(temp)
+    fullcsv=pd.concat(csvlist)
+    return fullcsv
+
+
+#%%            
+if __name__ == '__main__':
+    args=parseArguments()
+    path=args.dir
+    concat_csv(path)
+    
+    print(args)            
